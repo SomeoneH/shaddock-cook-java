3bed3b7793fe0598ed9099b96730a103ab6e3e9c
SomeoneH <373713100@qq.com>
2024-04-08 23:13:23 +0800

    工程初始化

---
+++ src/main/java/com/shaddock/shaddockcookjava/ShaddockCookJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shaddock.shaddockcookjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaddockCookJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaddockCookJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shaddock/shaddockcookjava/ShaddockCookJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaddock.shaddockcookjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaddockCookJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
