392a91e1385f3146be1ffe2942bfcf31abba2847
SomeoneH <373713100@qq.com>
2024-04-19 00:22:47 +0800

    用户信息VO问题修复

--- src/main/java/com/shaddock/api/app/vo/UserVo.java
+++ src/main/java/com/shaddock/api/app/vo/UserVo.java
@@ -1,12 +1,20 @@
 package com.shaddock.api.app.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Data
 public class UserVo implements Serializable {
-    private Integer followersCount;//粉丝数
-    private Integer subscribeCount;//关注数
-    private Integer momentCount;//动态数
+    private Long userId; //主键ID
+    private String loginName; //用户名
+    private String nickName; //昵称
+    private String introduceSign; //个性签名
+    private String userPic; //用户头像地址
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;//创建时间
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime updateTime;//更新时间
 }
